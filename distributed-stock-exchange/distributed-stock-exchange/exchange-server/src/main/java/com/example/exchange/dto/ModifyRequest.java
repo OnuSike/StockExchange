7f485e2e988eaf3d31331491cc915e8669027cfb
OnuSike <117122235+OnuSike@users.noreply.github.com>
2025-12-12 16:47:11 +0200

    Commit prj2 week11

---
+++ distributed-stock-exchange/distributed-stock-exchange/exchange-server/src/main/java/com/example/exchange/dto/ModifyRequest.java
@@ -0,0 +1,8 @@
+package com.example.exchange.dto;
+
+public class ModifyRequest {
+    private double newPrice;
+    public ModifyRequest() {}
+    public double getNewPrice() { return newPrice; }
+    public void setNewPrice(double newPrice) { this.newPrice = newPrice; }
+}
