046fc46212cbb10b235ac052d9541f868fd8d458
OnuSike <117122235+OnuSike@users.noreply.github.com>
2026-01-11 13:51:00 +0200

    Deleted distributed-stock-exchange/distributed-stock-exchange directory

--- distributed-stock-exchange/distributed-stock-exchange/exchange-server/src/main/java/com/example/exchange/dto/ModifyRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.exchange.dto;
-
-public class ModifyRequest {
-    private double newPrice;
-    public ModifyRequest() {}
-    public double getNewPrice() { return newPrice; }
-    public void setNewPrice(double newPrice) { this.newPrice = newPrice; }
-}
--- distributed-stock-exchange/distributed-stock-exchange/exchange-server/src/main/java/com/example/exchange/engine/OrderType.java
+++
@@ -1,5 +0,0 @@
-package com.example.exchange.engine;
-
-public enum OrderType {
-    BUY, SELL
-}
